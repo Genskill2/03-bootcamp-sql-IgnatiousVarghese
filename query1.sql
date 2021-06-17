387c46f7f64d9648db6524a57c222e69c38b6522
IgnatiousVarghese <ignatiousperukilath@gmail.com>
2021-06-17 11:44:57 +0530

    Done

---
+++ query1.sql
@@ -0,0 +1,4 @@
+select b.title 
+from publisher p,books b 
+where p.id=b.publisher 
+and p.name='PHI';
---
+++ query2.sql
@@ -0,0 +1,4 @@
+select b.title,p.name 
+from publisher p,books b 
+where b.publisher=p.id 
+and p.country='UK';
